bfd8bbd73a7566784b8cd840b9c67d223c468b02
AUEZ Developer <auez@dev.local>
2026-02-22 02:02:59 +0500

    Fix MongoDB connection string for Render

--- server/env.js
+++ server/env.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // MongoDB Atlas Connection String - using correct cluster
-process.env.MONGODB_URI = 'mongodb+srv://admin:183349@cluster0.xkfg5yg.mongodb.net/auezdb';
+process.env.MONGODB_URI = 'mongodb+srv://admin:183349@cluster0.xkfg5yg.mongodb.net/auezdb?retryWrites=true&w=majority';
 
 // Server Configuration
 process.env.PORT = process.env.PORT || '10000';
