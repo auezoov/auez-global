73249392e77e3d826dd9a60f0bf80cdbb50d7563
AUEZ Developer <auez@dev.local>
2026-02-22 04:33:00 +0500

    Sync server with official Telegram Token and Bot Name

--- server/env.js
+++ server/env.js
@@ -7,3 +7,7 @@ process.env.MONGODB_URI = 'mongodb+srv://admin:183349@cluster0.xkfg5yg.mongodb.n
 // Server Configuration
 process.env.PORT = process.env.PORT || '10000';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Telegram Bot Configuration
+process.env.TELEGRAM_BOT_TOKEN = '8404137291:AAHEyIuigot3KraCJNgU5a5DQrg3d9asYbs';
+process.env.TELEGRAM_BOT_NAME = 'Auez_club_bot';
