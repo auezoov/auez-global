067867905ef43600fd0f936f8442c9fc405926ce
AUEZ Developer <auez@dev.local>
2026-02-22 00:47:54 +0500

    Hard redeploy to fix 500 error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// cache bust - hard redeploy fix 500 error
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
