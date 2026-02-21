7f043d10bb84242899c0312fc72a0a03ffe3d11a
AUEZ Developer <auez@dev.local>
2026-02-21 18:18:14 +0500

    DEBUG MODE: Test Node.js basic functionality

---
+++ debug-check.js
@@ -0,0 +1,6 @@
+console.log("DEBUG: NODE VERSION", process.version);
+console.log("DEBUG: PLATFORM", process.platform);
+console.log("DEBUG: ARCH", process.arch);
+console.log("DEBUG: NODE ENV", process.env.NODE_ENV);
+console.log("DEBUG: WORKING DIR", process.cwd());
+console.log("DEBUG: SCRIPT SUCCESSFUL!");
